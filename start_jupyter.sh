b251ce74846bf578875741fd9bad143fd8fe77e9
Peter Bryzgalov <peterbryz@yahoo.com>
2016-09-18 05:16:55 +0900

    Docker image tag is parameter to start script. Advanced to model training. Cannot print out y value.

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -5,7 +5,11 @@ echo "Starting Jupyter in Docker mounting current folder inside container."
 echo "It will be available at http://localhost:8888 address."
 
 MOUNT_DIR=/notebooks
-IMAGE=tensorflow/tensorflow:0.10.0
+IMAGE_REPO=tensorflow/tensorflow
+if [[ $1 ]]; then
+	IMAGE_TAG=":$1"
+fi
+IMAGE=$IMAGE_REPO$IMAGE_TAG
 NAME=synt_tensor
 
 docker run -d -p 8888:8888 --name $NAME -v $(pwd):$MOUNT_DIR $IMAGE
