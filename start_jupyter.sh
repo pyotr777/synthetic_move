094a529764b115873c1111be5ac098801ccaa2cb
Peter Bryzgalov <peterbryz@yahoo.com>
2016-10-11 23:29:15 +0900

    Done: weights initialisation, forward propagation, hypothesis and cost function.

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -4,14 +4,14 @@
 echo "Starting Jupyter in Docker mounting current folder inside container."
 echo "It will be available at http://localhost:8888 address."
 
-MOUNT_DIR=/notebooks
-IMAGE_REPO=tensorflow/tensorflow
+MOUNT_DIR=/root
+IMAGE_REPO=pyotr777/jupyter
 if [[ $1 ]]; then
 	IMAGE_TAG=":$1"
 fi
 IMAGE=$IMAGE_REPO$IMAGE_TAG
 NAME=pyotr777_synth #synt_tensor
 
-docker run -d -p 8888:8888 --name $NAME -v $(pwd):$MOUNT_DIR $IMAGE
+docker run -ti -p 8888:8888 --name $NAME -v $(pwd):$MOUNT_DIR $IMAGE /bin/bash
 sleep 5
 open http://localhost:8888
